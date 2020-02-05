ba152abd8241ef55f379f6869ceff9927fa2d4cc
irerin07 <counterchord89@gmail.com>
2020-02-05 23:27:41 +0900

    1

---
+++ src/InputSample.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputSample() {
+  return (
+    <div>
+      <input />
+      <button>reset</button>
+      <div>
+        <b>value: </b>
+        blagblah
+      </div>
+    </div>
+  );
+}
