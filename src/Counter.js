099061cededfc08f996dd31b5db0ade02ebbe7e6
irerin07 <counterchord89@gmail.com>
2020-02-08 00:01:58 +0900

    5

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+function reduce(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      throw new Error("Unhandled Action");
+  }
+}
 
 function Counter() {
-  const [number, setNumber] = useState(0);
-
+  const [number, dispatch] = useReducer(reduce, 0);
   const onIncrease = () => {
-    setNumber(prevNumber => prevNumber + 1); //함수형 업데이트. 최적화와 관련되어있다.
+    //setNumber(prevNumber => prevNumber + 1); //함수형 업데이트. 최적화와 관련되어있다.
+    dispatch({
+      type: "INCREMENT"
+    });
   };
   const onDecrease = () => {
-    setNumber(prevNumber => prevNumber - 1);
+    // setNumber(prevNumber => prevNumber - 1);
+    dispatch({
+      type: "DECREMENT"
+    });
   };
   return (
     <div>
--- src/UserList.js
+++ src/UserList.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const User = React.memo(function User({ user, onRemove, onToggle }) {
-  const { username, email, id, active } = user;
-  useEffect(() => {
-    //UI가 나타난 이후에 실행된다.
-    // console.log("component mount");
-    // return () => {
-    //   console.log("component unmount");
-    // };
-    console.log("set user value");
-    console.log(user);
-    return () => {
-      console.log("before user value changes");
-      console.log(user);
-    };
-  }, [user]);
+  // const { username, email, id, active } = user;
+  // useEffect(() => {
+  //   //UI가 나타난 이후에 실행된다.
+  //   // console.log("component mount");
+  //   // return () => {
+  //   //   console.log("component unmount");
+  //   // };
+  //   console.log("set user value");
+  //   console.log(user);
+  //   return () => {
+  //     console.log("before user value changes");
+  //     console.log(user);
+  //   };
+  // }, [user]);
   //user 값이 변할때마다 useEffect함수가 호출되고 바뀌기 직전에는 클리너 함수(return)가 호출된다.
 
   return (
     <div>
       <b
-        style={{ color: active ? "green" : "black", cursor: "pointer" }}
-        onClick={() => onToggle(id)}
+        style={{ color: user.active ? "green" : "black", cursor: "pointer" }}
+        onClick={() => onToggle(user.id)}
       >
-        {username}
+        {user.username}
       </b>
       &nbsp;
-      <span>({email})</span>
-      <button onClick={() => onRemove(id)}>delete</button>{" "}
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>delete</button>{" "}
       {/* id를 파라미터로넣어주기 위해 함수를 사용 */}
     </div>
   );
@@ -64,7 +64,4 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default React.memo(
-  UserList,
-  (prevProps, nextProps) => nextProps.users === prevProps.users
-);
+export default React.memo(UserList);
